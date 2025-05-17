dfe01f782a2ad12d71b85214b09e24a8e8ae5d49
forry <forry@jifeng.online>
2025-05-17 21:34:48 +0800

    自动截断15s

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import motetiaowu
 import genhuanbeijing
 import os
 import runninghub_task
-from upload import UploadFile
+from moviepy.editor import VideoFileClip
 
 
 if __name__ == "__main_22_":
@@ -31,8 +31,32 @@ if __name__ == "__main__":
     files = {}
     for image_file in image_files:
         image_path = os.path.join(image_dir, image_file)
+        ##获取文件大小
+        file_size = os.path.getsize(image_path)
+        if file_size > 1024 * 1024 * 10:
+            print("!!!!!! image ", image_file, " is too big")
+            continue
+
         #viddeo 跟 image 命名一样, 就是后缀不一样 是 mp4
         video_path = os.path.join(video_dir, image_file.replace(".png", ".mp4"))
+        if not os.path.exists(video_path):
+            print("！！！！video", video_path, " is not exist")
+            continue
+
+        ##视频文件的播放时长
+        clip = VideoFileClip(video_path)
+        duration = clip.duration
+        print("视频时长:", duration)
+        raw_video_path = video_path.replace(".mp4", "_raw.mp4")
+        os.rename(video_path, raw_video_path)
+
+        if duration > 18:
+            print("视频时长大于18秒，截取3-18秒")
+            ## 使用ffmpeg 提取前15秒
+            os.system(f"/usr/local/bin/ffmpeg -ss 3  -i {raw_video_path}  -t  15 -c copy  -avoid_negative_ts 1  {video_path}")
+        else:
+            os.system(f"/usr/local/bin/ffmpeg -ss 3  -i {raw_video_path}  -t 15  -c copy  -avoid_negative_ts 1  {video_path}")
+
         print("\n\n")
         print(image_path)
         print(video_path)
